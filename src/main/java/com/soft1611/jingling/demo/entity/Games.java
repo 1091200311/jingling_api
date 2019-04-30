1f7484e4b8414988bdc2627d08d1bf99b10c6391
1091200311 <1091200311@qq.com>
2019-04-30 13:04:42 +0800

    second commit

---
+++ src/main/java/com/soft1611/jingling/demo/entity/Games.java
@@ -0,0 +1,21 @@
+package com.soft1611.jingling.demo.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+public class Games {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String pic;
+    private String name;
+    private String desc;
+}
---
+++ src/main/java/com/soft1611/jingling/demo/service/EducationService.java
@@ -0,0 +1,4 @@
+package com.soft1611.jingling.demo.service;
+
+public class EducationService {
+}
